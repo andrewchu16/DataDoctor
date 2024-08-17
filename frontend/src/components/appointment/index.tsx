f452d4b2faa7bfbf7748edaf714fbf225c9ba793
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2024-08-17 18:08:27 -0400

    Add input and appointment object

---
+++ frontend/src/components/appointment/index.tsx
@@ -0,0 +1,9 @@
+const Appointment = () => {
+    return (
+        <div className="appointment">
+            <h1>Appointment</h1>
+        </div>
+    );
+};
+
+export default Appointment;
---
+++ frontend/src/components/input/index.tsx
@@ -0,0 +1,5 @@
+const Input = () => {
+    return <div className="input"></div>;
+};
+
+export default Input;
