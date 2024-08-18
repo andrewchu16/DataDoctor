c21b726342d60a7ded61d85429cb9732bb5ce083
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2024-08-18 06:26:57 -0400

    Add landing page

--- frontend/src/pages/landingPage/index.tsx
+++ frontend/src/pages/landingPage/index.tsx
@@ -1,29 +1,28 @@
-import Appointment from "../../components/appointment";
-import Nav from "../../components/nav";
-import { AppointmentData } from "../../objects/AppointmentData";
-import { DialogueData } from "../../objects/DialogueData";
+import Button from "../../components/button";
+import TopRight from "../../assets/TopRight.svg";
+import Left from "../../assets/Left.svg";
+import BottomRight from "../../assets/BottomRight.svg";
+import "./style.css";
 
 const LandingPage = () => {
     return (
         <div className="landing_page">
-            <Appointment
-                appointment={
-                    new AppointmentData(
-                        0,
-                        0,
-                        0,
-                        0,
-                        0,
-                        "asd",
-                        "Name",
-                        "Virtual",
-                        "Summary",
-                        [new DialogueData("0", "Text", 123)],
-                        "Accepted"
-                    )
-                }
-            />
-            <Nav />
+            <img className="landing_page_art topright" src={TopRight} />
+            <img className="landing_page_art left" src={Left} />
+            <img className="landing_page_art bottomright" src={BottomRight} />
+            <div className="landing_page_content">
+                <span className="landing_page_title">
+                    Bridging families and doctors together.
+                </span>
+                <span className="landing_page_subtitle">
+                    I dunno what to write here guys.
+                    <br />
+                    Please help.
+                </span>
+                <Button icon="" type="primary">
+                    Get Started
+                </Button>
+            </div>
         </div>
     );
 };
