817ba1b88a1f89800c4c254c095e4b38737d8f17
ylabidi11 <youssef.labidi35@gmail.com>
2024-10-26 17:18:50 -0400

    oysg

--- backend/convoToTranscript.py
+++ backend/convoToTranscript.py
@@ -1,6 +1,6 @@
 import assemblyai as aai
 
-aai.settings.api_key = "a704c4c6727449e5bb4fba4d5d606551"
+aai.settings.api_key = ""
 transcriber = aai.Transcriber()
 
 transcript = transcriber.transcribe("./demoVideo.mp4")
